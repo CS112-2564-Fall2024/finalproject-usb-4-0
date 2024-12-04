64bd3d55b7ec63df9312aaf42d3099a85e6fa77c
Andrew Sorem <andrewsorem9@gmail.com>
2024-12-04 14:05:39 -0800

    Commit(2pm-12/4)

--- src/main/java/edu/miracosta/cs112/finalproject/finalproject/ExclusiCarMain.java
+++ src/main/java/edu/miracosta/cs112/finalproject/finalproject/ExclusiCarMain.java
@@ -4,6 +4,11 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.StackPane;
+
 
 import java.io.IOException;
 
@@ -14,6 +19,11 @@ public class ExclusiCarMain extends Application {
         Scene scene = new Scene(fxmlLoader.load(), 600, 375);
         stage.setScene(scene);
         stage.show();
+
+        Truck truck = new Truck(5, 800, "Titanium", "Commander", 3000, 140, 100.00, 6900);
+        EVSUV suv = new EVSUV(8, 600, "NovaFlux", "Aurora", 2999, 169, 120.00, 50, 30);
+
+
     }
 
     public static void main(String[] args) { launch(); }
--- src/main/java/edu/miracosta/cs112/finalproject/finalproject/Main.java
+++
@@ -1,9 +0,0 @@
-package edu.miracosta.cs112.finalproject.finalproject;
-
-public class Main {
-    Truck truck = new Truck(5, 800, "Titanium", "Commander", 3000, 140, 100.00, 6900;)
-    EVSUV suv = new EVSUV(8, 600, "NovaFlux", "Aurora", 2999, 169, 120.00, 50, 30);
-
-
-
-}
