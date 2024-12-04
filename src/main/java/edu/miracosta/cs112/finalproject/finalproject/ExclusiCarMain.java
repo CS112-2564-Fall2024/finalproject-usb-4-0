93c733bd1e14b1a3c9f836ffb1aec736aa2ed138
Andrew Sorem <andrewsorem9@gmail.com>
2024-12-04 14:23:39 -0800

    Commit(223pm-12/4)

--- src/main/java/edu/miracosta/cs112/finalproject/finalproject/ExclusiCarMain.java
+++ src/main/java/edu/miracosta/cs112/finalproject/finalproject/ExclusiCarMain.java
@@ -3,26 +3,48 @@ package edu.miracosta.cs112.finalproject.finalproject;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.SplitPane;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 import javafx.scene.control.Button;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.StackPane;
-
 
 import java.io.IOException;
 
 public class ExclusiCarMain extends Application {
     @Override
     public void start(Stage stage) throws IOException {
+
+        AnchorPane topPane = new AnchorPane();
+        topPane.setStyle("-fx-background-color: #ADD8E6;"); // Light blue background
+
+        AnchorPane mainPane = new AnchorPane();
+        mainPane.setStyle("-fx-background-color: #FFC0CB;"); // Pink background
+
+        SplitPane ui = new SplitPane();
+        ui.setOrientation(javafx.geometry.Orientation.VERTICAL);
+        ui.getItems().addAll(topPane, mainPane);
+        ui.setDividerPositions(0.2);
+
         FXMLLoader fxmlLoader = new FXMLLoader(ExclusiCarMain.class.getResource("main-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 600, 375);
+        Scene scene = new Scene(ui, 600, 375);
         stage.setScene(scene);
         stage.show();
 
         Truck truck = new Truck(5, 800, "Titanium", "Commander", 3000, 140, 100.00, 6900);
         EVSUV suv = new EVSUV(8, 600, "NovaFlux", "Aurora", 2999, 169, 120.00, 50, 30);
 
+        Button homeButton = new Button("Home");
+        homeButton.setFont(new Font("Arial", 30));
+        topPane.getChildren().add(homeButton);
+
+        Button aboutUsButton = new Button("About Us");
+        aboutUsButton.setFont(new Font("Arial", 30));
+        topPane.getChildren().add(aboutUsButton);
+
+        Button contactButton = new Button("Contact");
+        contactButton.setFont(new Font("Arial", 30));
+        topPane.getChildren().add(contactButton);
 
     }
 
