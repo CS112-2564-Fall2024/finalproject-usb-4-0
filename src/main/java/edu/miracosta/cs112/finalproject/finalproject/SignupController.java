af55bf7f5119d405c4fcfb518d1152e7c531f911
Andrew Sorem <andrewsorem9@gmail.com>
2024-12-06 23:42:55 -0800

    Commit(11:42pm-12/6)

--- src/main/java/edu/miracosta/cs112/finalproject/finalproject/SignupController.java
+++ src/main/java/edu/miracosta/cs112/finalproject/finalproject/SignupController.java
@@ -5,13 +5,29 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import java.io.IOException;
 
 public class SignupController {
 
+    User user = new User();
     @FXML
     Label signupTitleLabel;
+    @FXML
+    TextField firstNameField;
+    @FXML
+    TextField lastNameField;
+    @FXML
+    TextField emailField;
+    @FXML
+    TextField licenseNumberField;
+    @FXML
+    TextField dobYearField;
+    @FXML
+    TextField dobMonthField;
+    @FXML
+    TextField dobDayField;
 
     public void initialize() {
         signupTitleLabel.setText("Sign Up");
@@ -48,6 +64,16 @@ public class SignupController {
     }
     @FXML
     private void handleSubmitButton() {
+        user.setFirstName(firstNameField.getText());
+        user.setLastName(lastNameField.getText());
+        user.setEmail(emailField.getText());
+        user.setDriversLicenseNum(licenseNumberField.getText());
+        user.setBirthYear(Integer.parseInt(dobYearField.getText()));
+        user.setBirthMonth(Integer.parseInt(dobMonthField.getText()));
+        user.setBirthDay(Integer.parseInt(dobDayField.getText()));
+
+        handleButtonAction("home-view.fxml", signupTitleLabel);
+
 
     }
 
